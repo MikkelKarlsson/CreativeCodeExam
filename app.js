c00a5730939a07e0cd485333db39ba1d6d31094e
MikkelKarlsson <55698695+MikkelKarlsson@users.noreply.github.com>
2021-05-26 12:54:37 +0200

    restructure

---
+++ app.js
@@ -0,0 +1,29 @@
+    // Importing Express
+    const express = require('express');
+
+    // Instantiate Express
+    const app = express();
+
+    const path = require('path');
+
+    const fs = require('fs');
+
+    app.use(express.static("public"))
+
+    // All API's served to the html pages
+    app.get('/', (req, res) => {
+        res.sendFile(`${__dirname}/public/index.html`);
+    });
+
+
+
+//Define port
+const port = 8080;
+
+//Listenen to the port
+app.listen(port, (error) => {
+    if(error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", port);
+});
